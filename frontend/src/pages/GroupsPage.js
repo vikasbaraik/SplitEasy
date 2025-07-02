4e7c8570ae16824b1662dcdf1897d6331afdea02
Vikas <vbaraik3@gmail.com>
2025-07-02 07:45:05 +0530

    Remove .env file from repository and add to .gitignore Update frontend to use backend from .env

--- frontend/src/pages/GroupsPage.js
+++ frontend/src/pages/GroupsPage.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/';
+console.log('API_BASE_URL:', API_BASE_URL);
+
 function GroupsPage() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:5000/')
+    axios.get(API_BASE_URL)
       .then(response => setMessage(response.data))
       .catch(error => console.error(error));
   }, []);
